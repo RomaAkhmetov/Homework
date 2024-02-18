d9a82bd2f04546559cfa68c7771c00d69b69f1db
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-18 15:10:14 +0300

    hm 818
    https://acmp.ru/?main=task&id_task=818

---
+++ 818.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector <int> a(n);
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+
+		sum += a[i];
+	}
+	int z = n - 1;
+	int res = sum - z;
+	cout << res;
+}
