61596f844031b48c8a793c41071a5ae38ebe042c
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-18 14:47:38 +0300

    hw539
    https://acmp.ru/?main=task&id_task=539

---
+++ 539.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0)
+	{
+		cout << n / 2;
+	}
+	else if(n == 1)
+	{
+		cout << 0;
+	}
+	else
+	{
+		cout << n;
+	}
+}
