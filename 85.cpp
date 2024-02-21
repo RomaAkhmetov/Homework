ad91953c1297a3806bde105b3c948aae07ba7683
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-21 14:09:11 +0300

    https://acmp.ru/?main=task&id_task=85

---
+++ 85.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+
+	int mn = min(m, n);
+	int mx = max(m, n);
+	int d1 = 0;
+	for (int i = 1; i <= mn; i++)
+	{
+		if (mn % i == 0)
+		{
+			if (mx % i == 0)
+				d1 = i;
+		}
+	}
+
+	cout << d1;
+}
