15b963f674c0a00b539916c26ababb170b04873a
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-21 13:31:15 +0300

    Update 79.cpp

--- 79.cpp
+++ 79.cpp
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -16,21 +16,75 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 
-
-
-
 int main()
 {
 	int a, b;
 	cin >> a >> b;
-	
-	ull res = pow(a, b);
-	if (res == 0 || res == 1 || res == 2 || res == 3 || res == 4 || res == 5 || res == 6 || res == 7 || res == 8 || res == 9)
-	{
-		cout << res;
-	}
+
+	int ed = a % 10;
+	if (ed == 0 || ed == 1 || ed == 5 || ed == 6)
+		cout << ed;
 	else
 	{
-		cout << res % 10;
+		if (ed == 4 && (b % 2 != 0))
+			cout << 4;
+		else if(ed == 4 && (b % 2 == 0))
+			cout << 6;
+		else if (ed == 9 && (b % 2 != 0))
+			cout << 9;
+		else if (ed == 9 && (b % 2 == 0))
+			cout << 1;
+		else if (ed == 3)
+		{
+			int step = b / 4;
+			int ti = b - 4 * step;
+			if (ti == 1)
+				cout << 3;
+			else if (ti == 2)
+				cout << 9;
+			else if (ti == 3)
+				cout << 7;
+			else if (ti == 0)
+				cout << 1;
+		}
+		else if (ed == 7)
+		{
+			int step = b / 4;
+			int ti = b - 4 * step;
+			if (ti == 1)
+				cout << 7;
+			else if (ti == 2)
+				cout << 9;
+			else if (ti == 3)
+				cout << 3;
+			else if (ti == 0)
+				cout << 1;
+		}
+		else if (ed == 8)
+		{
+			int step = b / 4;
+			int ti = b - 4 * step;
+			if (ti == 1)
+				cout << 8;
+			else if (ti == 2)
+				cout << 4;
+			else if (ti == 3)
+				cout << 2;
+			else if (ti == 0)
+				cout << 6;
+		}
+		else if (ed == 2)
+		{
+			int step = b / 4;
+			int ti = b - 4 * step;
+			if (ti == 1)
+				cout << 2;
+			else if (ti == 2)
+				cout << 4;
+			else if (ti == 3)
+				cout << 8;
+			else if (ti == 0)
+				cout << 6;
+		}
 	}
 }
