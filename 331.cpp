e1c4c5e8962844ff4a62d76f95b55468cb976cb6
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-22 20:53:31 +0300

    https://acmp.ru/?main=task&id_task=331

---
+++ 331.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	int th, tm;
+	cin >> th >> tm;
+
+	string hs = s.substr(0, 2);
+	string ms = s.substr(s.find(":")+1, 2);
+	int h = stoi(hs);
+	int m = stoi(ms);
+	
+	int sum1 = h + th;
+	int sum2 = m + tm;
+	
+	int summin = sum1 * 60 + sum2;
+	
+	int days = summin / 1440;
+	int resh = summin / 60 - days * 24;
+	
+	int resm = summin % 1440 - resh * 60;
+
+	cout << setw(2) << setfill('0') << resh << ":" << setw(2) << setfill('0') << resm;
+
+}
