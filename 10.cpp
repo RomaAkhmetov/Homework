62df7875fa7d8be316ece6aac9d91a84e4a8b8b9
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-22 15:02:23 +0300

    https://acmp.ru/?main=task&id_task=10

---
+++ 10.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	
+	for (int i = -100; i <= 100; i++)
+	{
+		if ((a * pow(i, 3) + b * pow(i, 2) + c * i + d) == 0)
+			cout << i << ' ';
+	}
+}
