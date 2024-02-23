2e6d8bae2015c819a289baf3188c1688cbdeae49
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-23 19:30:39 +0300

    https://acmp.ru/?main=task&id_task=94

---
+++ 94.cpp
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	if(n < m && n <= k)
+		cout << "NO";
+	else
+	{
+		int cnt = 0;
+		while (m > 0)
+		{
+			if (m > 0)
+			{
+				m = m - n;
+				cnt++;
+				if (m <= 0) {
+					cout << cnt;
+					return 0;
+				}
+				else
+					m += k;
+			}
+		}
+		cout << cnt;
+	}
+}
