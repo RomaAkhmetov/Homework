811875dfd70780b5d3a3d9a3c9a67b09486268e7
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-23 17:16:02 +0300

    https://acmp.ru/?main=task&id_task=554

---
+++ 554.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int sliece_num = 2;
+	for (int i = 2; i <= n; i++)
+	{
+		sliece_num += i;
+	}
+	cout << sliece_num;
+}
