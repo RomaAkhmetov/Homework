12d312a96983f524a90fdabfdef758cda638c9da
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-28 20:49:39 +0300

    https://acmp.ru/index.asp?main=task&id_task=66

---
+++ 66.cpp
@@ -0,0 +1,33 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	char letter;
+	cin >> letter;
+
+	vector <char> klaviatura { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b',
+	'n', 'm' };
+	auto it = find(klaviatura.begin(), klaviatura.end(), letter);
+	auto index = distance(klaviatura.begin(), it);
+
+	if (letter == 'm')
+		cout << 'q';
+	else
+		cout << klaviatura[index + 1];
+}
