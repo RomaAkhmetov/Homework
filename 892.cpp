85f9db012f437083a2d81ba5e4d5bc432f1acb45
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-28 19:37:38 +0300

    https://acmp.ru/index.asp?main=task&id_task=892

---
+++ 892.cpp
@@ -0,0 +1,34 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	if (N == 1 || N == 2 || N == 12)
+		cout << "Winter";
+	else if (N == 3 || N == 4 || N == 5)
+		cout << "Spring";
+	else if (N == 6 || N == 7 || N == 8)
+		cout << "Summer";
+	else if (N == 9 || N == 10 || N == 11)
+		cout << "Autumn";
+	else if (N > 12)
+		cout << "Error";
+}
