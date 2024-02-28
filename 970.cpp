d58e25ed0c59f337848b006677cddc035a5ca9ae
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-28 19:29:01 +0300

    https://acmp.ru/index.asp?main=task&id_task=970

---
+++ 970.cpp
@@ -0,0 +1,28 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int a1, a2, a3;
+	cin >> a1 >> a2 >> a3;
+
+	if (((a1 + a2) == a3) || ((a1 + a3) == a2) || ((a2 + a3) == a1))
+		cout << "YES";
+	else
+		cout << "NO";
+}
