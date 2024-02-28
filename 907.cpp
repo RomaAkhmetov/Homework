e784e87294c2cf36e682542690b0d6f18c5dd930
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-28 15:40:20 +0300

    https://acmp.ru/index.asp?main=task&id_task=907

---
+++ 907.cpp
@@ -0,0 +1,28 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	double w, h, r;
+	cin >> w >> h >> r;
+
+	if (2 * r <= min(w, h))
+		cout << "YES";
+	else
+		cout << "NO";
+}
