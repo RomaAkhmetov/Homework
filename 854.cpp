bac3e7520280fd3e0574b65ac94c430396443f50
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-29 18:09:57 +0300

    https://acmp.ru/index.asp?main=task&id_task=854

---
+++ 854.cpp
@@ -0,0 +1,35 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int tr, tc;
+	cin >> tr >> tc;
+
+	string s;
+	cin >> s;
+
+	if (s == "freeze")
+		cout << min(tr, tc);
+	else if (s == "heat")
+		cout << max(tr, tc);
+	else if (s == "fan")
+		cout << tr;
+	else if (s == "auto")
+		cout << tc;
+}
