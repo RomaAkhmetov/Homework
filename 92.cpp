43a0bf98d4e1afcd3c6082b67cc53969d3854180
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-29 17:09:08 +0300

    https://acmp.ru/index.asp?main=task&id_task=92

---
+++ 92.cpp
@@ -0,0 +1,27 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int S;
+	cin >> S;
+
+	double x = S / 6;
+	double k = 4 * x;
+	cout << x << ' ' << k << ' ' << x;
+}
