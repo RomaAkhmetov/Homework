c82c495983f8b691b66aa34dfc7c13c3fa9d96a0
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-02-29 16:32:29 +0300

    https://acmp.ru/index.asp?main=task&id_task=106

---
+++ 106.cpp
@@ -0,0 +1,40 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector <int> moneys(n);
+	int r = 0;
+	int o = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> moneys[i];
+
+		if (moneys[i] == 1)
+			r++;
+		else
+			o++;
+	}
+	if (r > o)
+		cout << o;
+	else
+		cout << r;
+}
