eab27d65af55ebf13d5a29a8799a42c0a78e389d
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-01 16:03:08 +0300

    https://acmp.ru/index.asp?main=task&id_task=940

---
+++ 940.cpp
@@ -0,0 +1,29 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int k;
+	cin >> k;
+
+	string s;
+	cin >> s;
+
+	s.erase(k - 1, 1);
+	cout << s;
+}
