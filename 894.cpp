371755c5682c37766b20d3eed834a11b0e936725
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-02 17:24:14 +0300

    https://acmp.ru/index.asp?main=task&id_task=894

---
+++ 894.cpp
@@ -0,0 +1,28 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	double S, r1;
+	cin >> S >> r1;
+	
+	double z_r2 = sqrt((M_PI * pow(r1, 2) - S) / M_PI);
+	cout << round(z_r2 * 1000.0) / 1000.0;
+}
