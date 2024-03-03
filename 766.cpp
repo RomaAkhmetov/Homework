ec5adbe7ea5e43865f53312f1c26edb0e009cf55
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-03 19:05:08 +0300

    https://acmp.ru/index.asp?main=task&id_task=766

---
+++ 766.cpp
@@ -0,0 +1,29 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	if (n * m >= k)
+		cout << "YES";
+	else
+		cout << "NO";
+}
