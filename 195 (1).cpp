4d25efd191f10cb03a3005746917ebe361a7e253
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-04 18:10:04 +0300

    https://acmp.ru/index.asp?main=task&id_task=195

---
+++ 195 (1).cpp
@@ -0,0 +1,27 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	int n, a, b;
+	cin >> n >> a >> b;
+
+	int res = (n * (a * b)) * 2;
+	cout << res;
+}
