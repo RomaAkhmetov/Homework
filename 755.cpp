d7abc0b3aa87c1159c8b67c10a72ddee9fc4cd20
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-05 16:53:46 +0300

    https://acmp.ru/index.asp?main=task&id_task=755

---
+++ 755.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	int x, y, z;
+	cin >> x >> y >> z;
+
+	int sum = x + y;
+	if (sum >= z)
+		cout << sum - z;
+	else
+		cout << "Impossible";
+}
