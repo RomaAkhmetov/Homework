0fa7379086417b2cefeab9d691031bac291027bb
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-05 15:04:02 +0300

    Update 822.cpp

--- 822.cpp
+++ 822.cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES 
+#define _USE_MATH_DEFINES 
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -17,23 +17,23 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-double Fd(int px, int py, int px2, int py2)
+double fd(double px, double py, double px2, double py2)
 {
-	int S1 = abs(px - px2);
-	int S2 = abs(py - py2);
+	double S1 = abs(px - px2);
+	double S2 = abs(py - py2);
 	double t = sqrt(pow(S1, 2) + pow(S2, 2));
 	return t;
 }
 
 int main()
 {
-	int x1, y1, x2, y2, x3, y3;
+	double x1, y1, x2, y2, x3, y3;
 	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
 
-	double a = Fd(x1, y1, x2, y2);
-	double b = Fd(x1, y1, x3, y3);
-	double c = Fd(x2, y2, x3, y3);
+	double a = fd(x1, y1, x2, y2);
+	double b = fd(x1, y1, x3, y3);
+	double c = fd(x2, y2, x3, y3);
 	double p = (a + b + c) / 2.0;
 	double Str = sqrt(p * (p - a) * (p - b) * (p - c));
-	cout << Str;
+	cout << fixed << setprecision(1) << Str;
 }
