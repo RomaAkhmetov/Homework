056d571f4d7eb22956780f4277ece65850da7fce
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-07 18:35:36 +0300

    https://acmp.ru/index.asp?main=task&id_task=81

---
+++ 81.cpp
@@ -0,0 +1,33 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector <int> wm(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> wm[i];
+	}
+	sort(wm.begin(), wm.end());
+	cout << wm[0] << ' ' << wm[n - 1];
+}
