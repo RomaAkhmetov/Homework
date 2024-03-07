18a64646b75e649982a22349b0fca3501a73d903
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-07 18:29:52 +0300

    https://acmp.ru/index.asp?main=task&id_task=312

---
+++ 312.cpp
@@ -0,0 +1,29 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	int a1, a2, n;
+	cin >> a1 >> a2 >> n;
+
+	int d = a2 - a1;
+	int an = a1 + d * (n - 1);
+	cout << an;
+}
