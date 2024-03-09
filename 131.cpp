5f753f8abaa605afc664b117786c319a8c6119df
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-09 15:47:31 +0300

    https://acmp.ru/index.asp?main=task&id_task=131

---
+++ 131.cpp
@@ -0,0 +1,55 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+struct Pair1
+{
+	int V;
+	int S;
+};
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	vector <Pair1> p(n);
+	int male = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p[i].V >> p[i].S;
+	}
+
+
+	int index = -2;
+	int age = -1;
+	for (int j = 0; j < n; j++)
+	{
+		if (p[j].S == 1)
+		{
+			if (p[j].V > age)
+			{
+				age = p[j].V;
+				index = j;
+			}
+		}
+	}
+	cout << index + 1;
+
+}
