5e3b5bf45e152a931d6e4ea596f98d356fa5071a
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-15 16:45:25 +0300

    https://acmp.ru/index.asp?main=task&id_task=25

---
+++ 25.cpp
@@ -0,0 +1,33 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll a, b;
+	cin >> a >> b;
+
+	if (b > a)
+		cout << '<';
+	else if (a > b)
+		cout << '>';
+	else if (a == b)
+		cout << '=';
+}                         
