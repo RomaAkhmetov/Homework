fcd8192f9b95124bcb5930f09e2ebf27dda8af3c
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-16 18:13:33 +0300

    Update 678.cpp

--- 678.cpp
+++ 678.cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES 
+#define _USE_MATH_DEFINES 
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -24,23 +24,29 @@ int main()
 	string s;
 	cin >> s;
 
-	int ballpossicion = 1;
+	vector <int> a = { 1, 2, 3 };
 	for (int i = 0; i < s.size(); i++)
 	{
-		if (s[i] == 'A' && ballpossicion == 1)
-			ballpossicion++;
-		if (s[i] == 'A' && ballpossicion == 2)
-			ballpossicion--;
-		else if (s[i] == 'B' && ballpossicion == 3)
-			ballpossicion--;
-		else if (s[i] == 'B' && ballpossicion == 2)
-			ballpossicion++;
-		else if (s[i] == 'C' && ballpossicion == 3)
-			ballpossicion -= 2;
-		else if (s[i] == 'C' && ballpossicion == 1)
-			ballpossicion += 2;
+		if (s[i] == 'A')
+			swap(a[0], a[1]);
+		else if (s[i] == 'B')
+			swap(a[1], a[2]);
+		else if (s[i] == 'C')
+			swap(a[0], a[2]);
 	}
-	cout << ballpossicion;
+	int res = 0;
+	for (int j = 0; j <= a.size(); j++)
+	{
+		if (a[j] == 1)
+		{
+			res = j + 1;
+			break;
+		}
+	}
+	cout << res;
 }
+
+
+
 
 
