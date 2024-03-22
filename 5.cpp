e19fe3944473da2b8221c67803a91bd378a2f5ba
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-22 16:52:43 +0300

    https://acmp.ru/index.asp?main=task&id_task=5

---
+++ 5.cpp
@@ -0,0 +1,70 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	vector <ll> m(n);
+	vector <ll> ch;
+	vector <ll> not_ch;
+	for (ll i = 0; i < m.size(); i++)
+	{
+		cin >> m[i];
+
+		if (m[i] % 2 == 0)
+			ch.push_back(m[i]);
+		else
+			not_ch.push_back(m[i]);
+	}
+
+	if (ch.size() > not_ch.size())
+	{
+		for (ll j = 0; j < not_ch.size(); j++)
+		{
+			cout << not_ch[j] << ' ';
+		}
+		cout << endl;
+		for (ll o = 0; o < ch.size(); o++)
+		{
+			cout << ch[o] << ' ';
+		}
+		cout << endl;
+		cout << "YES";
+	}
+	else
+	{
+		for (ll g = 0; g < not_ch.size(); g++)
+		{
+			cout << not_ch[g] << ' ';
+		}
+		cout << endl;
+		for (ll z = 0; z < ch.size(); z++)
+		{
+			cout << ch[z] << ' ';
+		}
+		cout << endl;
+		cout << "NO";
+	}
+}
+
+
