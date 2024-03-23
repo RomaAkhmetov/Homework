12c8b1fc4e2adf0cf7177daa4bbf9bee5a933f17
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-23 15:51:28 +0300

    https://acmp.ru/index.asp?main=task&id_task=839

---
+++ 839.cpp
@@ -0,0 +1,40 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	ll zero = 0;
+	for (ll i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '0')
+			zero++;
+	}
+
+	if (zero == 0)
+		cout << "YES";
+	else
+		cout << "NO";
+}
+
+
