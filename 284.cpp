60b0443016d2eaca28a210c89f2d88662ee8935a
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-25 20:32:42 +0300

    https://acmp.ru/index.asp?main=task&id_task=284

---
+++ 284.cpp
@@ -0,0 +1,58 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	vector <ll> a(n);
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	cout << ' ';
+
+	ll m; 
+	cin >> m;
+
+	for (ll i = 0; i < m; i++)
+	{
+		ll ik, jk;
+		cin >> ik >> jk;
+
+		vector <ll> res;
+		while (ik <= jk)
+		{
+			res.push_back(a[ik - 1]);
+			ik++;
+		}
+
+		for (ll o = 0; o < res.size(); o++)
+		{
+			cout << res[o] << ' ';
+		}
+		cout << endl;
+	}
+	
+}
+
+
