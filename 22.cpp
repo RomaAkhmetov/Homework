176f03891876662379fc49ad68473457ce4cbc49
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-28 20:04:05 +0300

    https://acmp.ru/index.asp?main=task&id_task=22

---
+++ 22.cpp
@@ -0,0 +1,49 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	vector <ll> ost;
+	ll a = 0;
+	while (n / 2 != 0)
+	{
+		a = n % 2;
+		ost.push_back(a);
+		n /= 2;
+		if (n == 1)
+		{
+			a = n % 2;
+			ost.push_back(a);
+		}
+    }
+	ll cnt = 0;
+	for (ll i = 0; i < ost.size(); i++)
+	{
+		if (ost[i] == 1)
+			cnt++;
+	}
+	cout << cnt;
+}
+
+
