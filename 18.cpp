09472e53f1c208e81a843d3a0e21737a3cfc5ff2
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-28 19:39:03 +0300

    https://acmp.ru/index.asp?main=task&id_task=18

---
+++ 18.cpp
@@ -0,0 +1,47 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ull n;
+	cin >> n;
+
+	if (n == 0)
+		cout << 1;
+	else
+	{
+		ull zs = 1 * n;
+		vector <ull> z;
+		ull pr = 1;
+		for (ull i = 2; i < n; i++)
+		{
+			z.push_back(i);
+		}
+		for (ull j = 0; j < z.size(); j++)
+		{
+			pr *= z[j];
+		}
+		ull res = zs * pr;
+		cout << res;
+	}
+}
+
+
