c39e5116074a792f4cf05b93fe6c7808a69c8537
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-03-31 17:32:50 +0300

    https://acmp.ru/index.asp?main=task&id_task=844

---
+++ 844.cpp
@@ -0,0 +1,37 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ull a, b;
+	cin >> a >> b;
+
+	ull z = a * b;
+	double res = sqrt(z);
+	double res2 = (int)res;
+	double res3 = abs(res - res2);
+	if (res3 > 0)
+		cout << 0;
+	else
+		cout << (int)res;
+}
+
+
