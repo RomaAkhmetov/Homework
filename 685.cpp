d85551995d6fe626869f3cfa9f08927fa41da805
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-04-08 17:23:37 +0300

    https://acmp.ru/index.asp?main=task&id_task=685

---
+++ 685.cpp
@@ -0,0 +1,36 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	ll a1, a2, a3, b1, b2, b3;
+	cin >> a1 >> a2 >> a3 >> b1 >> b2 >> b3;
+
+	vector <ll> a = { a1, a2, a3 };
+	vector <ll> b = { b1, b2, b3 };
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	ll mxa = a[a.size() - 1];
+	ll mda = a[1];
+	ll mna = a[0];
+	ll mxb = b[b.size() - 1];
+	ll mdb = b[1];
+	ll mnb = b[0];
+	ll res = mxa * mxb + mda * mdb + mna * mnb;
+	cout << res;
+}
