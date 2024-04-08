605e52cf02ba6ab81e5865c52f8540abc023487a
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-04-08 18:31:46 +0300

    https://acmp.ru/index.asp?main=task&id_task=297

---
+++ 297.cpp
@@ -0,0 +1,39 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	string n;
+	cin >> n;
+
+	ll cnt = 0;
+	for (ll i = 0; i < n.size(); i++)
+	{
+		if (n[i] == '0')
+			cnt += 1;
+		else if (n[i] == '6')
+			cnt += 1;
+		else if (n[i] == '8')
+			cnt += 2;
+		else if (n[i] == '9')
+			cnt += 1;
+		else
+			cnt += 0;
+	}
+	cout << cnt;
+}
