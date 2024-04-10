378f4cc25a20865f196b9f6a1c190e50094a4c89
RomaAkhmetov <160057959+RomaAkhmetov@users.noreply.github.com>
2024-04-10 20:26:29 +0300

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll Problem_of_Factorial(ll z)
+{
+	if (z == 0 || z == 1)
+		return 1;
+	return z * Problem_of_Factorial(z - 1);
+}
+
+int main()
+{
+	ll z;
+	cin >> z;
+
+	cout << Problem_of_Factorial(z);
+}
