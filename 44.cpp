8da2d55647619ef256b6d870316de184ec7867c6
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-04-18 20:25:41 +0300

    https://acmp.ru/index.asp?main=task&id_task=44

---
+++ 44.cpp
@@ -0,0 +1,35 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	string s;
+	cin >> s;
+	
+	ull cnt = 0;
+	for (ull i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '>' && s[i + 1] == '>' && s[i + 2] == '-' && s[i + 3] == '-' && s[i + 4] == '>')
+			cnt++;
+		else if (s[i] == '<' && s[i + 1] == '-' && s[i + 2] == '-' && s[i + 3] == '<' && s[i + 4] == '<')
+			cnt++;
+	}
+	cout << cnt;
+}
+
+//Z
