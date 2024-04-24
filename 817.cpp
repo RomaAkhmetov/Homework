8139c73d0f88af34a9ac064a789eccc000bc01d2
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-04-24 22:06:26 +0300

    https://acmp.ru/index.asp?main=task&id_task=817

---
+++ 817.cpp
@@ -0,0 +1,28 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	ll n, m, d, k;
+	cin >> n >> m >> d >> k;
+
+	ll res = (d * k * n + d * k * m) - (d * d * n * m);
+	cout << res;
+}
+
+//Z
