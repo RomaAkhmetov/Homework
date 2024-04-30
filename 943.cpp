5f5a9578eb849c2d9ad3a5773d43e523f06c2671
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-04-30 12:41:50 +0300

    https://acmp.ru/index.asp?main=task&id_task=943

---
+++ 943.cpp
@@ -0,0 +1,43 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n, m, y, x;
+	cin >> n >> m >> y >> x;
+	
+	vector <vector <ll>> a;
+	for (ll i = 0; i < n; i++)
+	{
+		vector <ll> g;
+		for (ll j = 0; j < m; j++)
+		{
+			if (i % 2 != 0)
+				g.push_back(i * m + (m - 1) - j);
+			else
+				g.push_back(i * m + j);
+		}
+		a.push_back(g);
+
+	}
+	cout << a[y - 1][x - 1];
+}
+
+//Z
