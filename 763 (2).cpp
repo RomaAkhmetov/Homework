5e71e37cc584c934e080371d7e70173186bda7c3
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-01 17:14:17 +0300

    https://acmp.ru/index.asp?main=task&id_task=763

---
+++ 763 (2).cpp
@@ -0,0 +1,55 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll x, y;
+	cin >> x >> y;
+
+	if (x == 1 && y != 1)
+	{
+		ll res = y - 1;
+		if (res % 2 == 0)
+			cout << 2;
+		else
+			cout << 1;
+	}
+	else if (x != 1 && y == 1)
+	{
+		ll r = x - 1;
+		if (r % 2 != 0)
+			cout << 1;
+		else
+			cout << 2;
+	}
+	else if (x != 1 && y != 1)
+	{
+		ll re = x - 1;
+		if (re % 2 == 0)
+			cout << 2;
+		else
+			cout << 1;
+	}
+	else if (x == 1 && y == 1)
+		cout << 0;
+	
+}
+
+//Z
