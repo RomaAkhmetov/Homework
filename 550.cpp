4c751f77e72407906f41f60fe4f78349c73443b5
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-03 17:00:17 +0300

    https://acmp.ru/index.asp?main=task&id_task=550

---
+++ 550.cpp
@@ -0,0 +1,38 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	if ((n % 400 == 0) || (n % 4 == 0 && n % 100 != 0))
+	{
+		cout << 12 << '/' << "09" << '/' << n;
+		return 0;
+	}
+	else
+	{
+		cout << 13 << '/' << "09" << '/' << n;
+		return 0;
+	}
+}
+
+//Z
