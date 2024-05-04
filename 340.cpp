0a79edf1994f400b99c96ca1409114d796eba348
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-04 16:30:05 +0300

    https://acmp.ru/index.asp?main=task&id_task=340

---
+++ 340.cpp
@@ -0,0 +1,39 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll a1, b1, c1;
+	cin >> a1 >> b1 >> c1;
+
+	ll a2, b2, c2;
+	cin >> a2 >> b2 >> c2;
+
+	if ((a1 == a2 && b1 == b2 && c1 == c2) || (a1 == b2 && b1 == a2 && c1 == c2) || (a1 == c2 && b1 == b2 && c1 == a2))
+		cout << "Boxes are equal";
+	else if ((a1 <= a2 && b1 <= b2 && c1 <= c2) || (a1 <= b2 && b1 <= a2 && c1 <= c2) || (a1 <= c2 && b1 <= b2 && c1 <= a2))
+		cout << "The first box is smaller than the second one";
+	else if ((a1 >= a2 && b1 >= b2 && c1 >= c2) || (a1 >= b2 && b1 >= a2 && c1 >= c2) || (a1 >= c2 && b1 >= b2 && c1 >= a2))
+		cout << "The first box is larger than the second one";
+	else
+		cout << "Boxes are incomparable";
+}
+
+//Z
