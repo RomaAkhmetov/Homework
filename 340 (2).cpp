3dc7b75b5f1d9d3e3c26d735a28a8692635e4af5
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-08 20:58:14 +0300

    Update 340 (2).cpp

--- 340 (2).cpp
+++ 340 (2).cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -21,36 +21,25 @@ typedef unsigned long long ull;
 
 int main()
 {
-	vector <ll> v(6);
-	for (ll i = 0; i < 6; i++)
-	{
-		cin >> v[i];
-	}
-	vector <ll> tv1;
-	vector <ll> tv2;
+	vector <ll> tv1(3);
+	vector <ll> tv2(3);
 	for (ll j = 0; j < 3; j++)
 	{
-		tv1.push_back(v[j]);
+		cin >> tv1[j];
 	}
-	for (ll u = 3; u < 6; u++)
+	for (ll u = 0; u < 3; u++)
 	{
-		tv2.push_back(v[u]);
+		cin >> tv2[u];
 	}
 
 	sort(tv1.begin(), tv1.end());
 	sort(tv2.begin(), tv2.end());
+
 	if (tv1[0] == tv2[0] && tv1[1] == tv2[1] && tv1[2] == tv2[2])
 		cout << "Boxes are equal";
-	else if (tv1[0] >= tv2[0] && tv1[2] >= tv2[2])
-	{
-		if ((tv1[0] == tv2[0] && tv1[2] == tv2[2] && tv1[1] > tv2[1]) || (tv1[1] == tv2[1] && tv1[2] == tv2[2] && tv1[0] > tv2[0]) || (tv1[1] == tv2[1] && tv1[2] > tv2[2] && tv1[0] == tv2[0]))
-			cout << "The first box is larger than the second one";
-		else
-			cout << "The first box is smaller than the second one";
-		return 0;
-		
-	}
-	else if (tv1[0] <= tv2[0] && tv1[2] <= tv2[2])
+	else if (tv1[0] >= tv2[0] && tv1[1] >= tv2[1] && tv1[2] >= tv2[2])
+		cout << "The first box is larger than the second one";
+	else if (tv1[0] <= tv2[0] && tv1[1] <= tv2[1] && tv1[2] <= tv2[2])
 		cout << "The first box is smaller than the second one";
 	else
 		cout << "Boxes are incomparable";
