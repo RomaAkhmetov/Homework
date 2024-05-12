e930533be79a16886909a14a0dc0127f11b58172
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-12 19:13:14 +0300

    https://codeforces.com/contest/1971/problem/D

---
+++ cd.cpp
@@ -0,0 +1,53 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll t;
+	cin >> t;
+
+	
+	for (ll i = 0; i < t; i++)
+	{
+		vector <char> v;
+
+		string s;
+		cin >> s;
+
+		char c = ' ';
+		ll p1 = 0;
+		for (ll j = 0; j < s.size(); j++)
+		{
+
+			char tmp = s[j];
+			if (tmp != c)
+			{
+				if (tmp == '1' && c == '0')
+					p1 = -1;
+				v.push_back(tmp);
+				c = tmp;
+			}
+		}
+		cout << v.size() + p1 << "\n";
+	}
+}
+
+//Z
