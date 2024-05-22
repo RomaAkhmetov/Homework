d387c95d55e78ea30c078e105dcad213f0460278
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-22 11:15:34 +0300

    Update 688.cpp

--- 688.cpp
+++ 688.cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -19,46 +19,49 @@ typedef unsigned long long ull;
  
 struct Point
 {
-	ll xh;
-	ll yh;
+    ll xh;
+    ll yh;
 };
 
 int main()
 {
-	ll xg, yg;
-	cin >> xg >> yg;
+    ll xg, yg;
+    cin >> xg >> yg;
 
-	ll xd, yd;
-	cin >> xd >> yd;
+    ll xd, yd;
+    cin >> xd >> yd;
 
-	ll n;
-	cin >> n;
+    ll n;
+    cin >> n;
 
-	vector <Point> a;
-	for (ll i = 0; i < n; i++)
-	{
-		Point tmp;
-		cin >> tmp.xh >> tmp.yh;
+    vector <Point> a;
+    for (ll i = 0; i < n; i++)
+    {
+        Point tmp;
+        cin >> tmp.xh >> tmp.yh;
 
-		a.push_back(tmp);
-	}
-	ll ind_r = -1;
-	for (ll o = 0; o < n; o++)
-	{
-		ll ag = abs(xg - a[o].xh);
-		ll bg = abs(yg - a[o].yh);
-		ll cg = sqrt(pow(ag, 2) + pow(bg, 2));
-		ll ad = abs(xd - a[o].xh);
-		ll bd = abs(yd - a[o].yh);
-		ll cd = sqrt(pow(ad, 2) + pow(bd, 2));
+        a.push_back(tmp);
+    }
+    ll ind_r = -1;
+    for (ll o = 0; o < n; o++)
+    {
+        ll ag = abs(xg - a[o].xh);
+        ll bg = abs(yg - a[o].yh);
+        double cg = sqrt(pow(ag, 2) + pow(bg, 2));
+        ll ad = abs(xd - a[o].xh);
+        ll bd = abs(yd - a[o].yh);
+        double cd = sqrt(pow(ad, 2) + pow(bd, 2));
 
-		if (cg < cd)
-			ind_r = o;
-	}
-	if (ind_r == -1)
-		cout << "NO";
-	else
-		cout << ind_r + 1;
+        if (cg <= (cd / 2.0))
+        {
+            ind_r = o;
+            break;
+        }
+    }
+    if (ind_r == -1)
+        cout << "NO";
+    else
+        cout << ind_r + 1;
 }
 
 //Z
