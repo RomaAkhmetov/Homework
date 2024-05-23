02224343ba78bfe5ecf70380455f58cd3ac18128
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-23 22:33:52 +0300

    https://acmp.ru/index.asp?main=task&id_task=757

---
+++ 757.cpp
@@ -0,0 +1,40 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+
+int main()
+{
+	ll c, h, o;
+	cin >> c >> h >> o;
+
+	if (c < 2 || h < 6 || o < 1)
+		cout << 0;
+	else
+	{
+		ll mn = min(c, min(h, o));
+		if (mn == h)
+			cout << h / 6;
+		else if (mn == c)
+			cout << c / 2;
+		else
+			cout << o;
+	}
+}
+
+//Z
