144f73744340199444c4b95bbd29e0b285dfafbf
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-26 16:13:31 +0300

    https://acmp.ru/index.asp?main=task&id_task=633

---
+++ 633.cpp
@@ -0,0 +1,43 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	string s1;
+	cin >> s1;
+
+	string s2;
+	cin >> s2;
+
+	string s3;
+	cin >> s3;
+
+	vector <string> v;
+	v.push_back(s1);
+	v.push_back(s2);
+	v.push_back(s3);
+	sort(v.begin(), v.end());
+	cout << s << ':' << ' ' << v[0] << ',' << ' ' << v[1] << ',' << ' ' << v[2];
+}
+
+//Z
