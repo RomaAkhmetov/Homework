9452d8112a0a4efa9137ac754273e83ce418252a
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-30 23:12:13 +0300

    Update 163.cpp

--- 163.cpp
+++ 163.cpp
@@ -1,4 +1,4 @@
-﻿#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
 #include <algorithm>
 #include <cmath>
 #include <vector>
@@ -17,68 +17,101 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
  
+char ctoi(char a)
+{
+	return a - '0';
+}
+
 int main()
 {
 	string s;
 	cin >> s;
 
-	vector <string> v;
+	vector <string> v = {""};
 	vector <string> nu;
 	vector <string> for_x;
 	for (ll i = 0; i < s.size(); i++)
 	{
-		if (s[i] == '=')
-			v.push_back(to_string(s[i]));
-		else
+		if (s[s.size() - 1] == 'x')
 		{
-			if (s[i] == 'x' && i == 0)
-				for_x.push_back("1");
-			else if (s[i + 1] == 'x' && s[i] == '+')
-				for_x.push_back("1");
-			else if (s[i + 1] == 'x' && s[i] == '-')
-				for_x.push_back("-1");
-			else if (s[i + 1] == 'x' && (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=') && i == 0)
-				for_x.push_back(to_string(s[i]));
-			else if (s[i + 1] == 'x' && (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=') && s[i - 1] == '+')
-				for_x.push_back(to_string(s[i]));
-			else if (s[i + 1] == 'x' && (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=') && s[i - 1] == '-')
-			{
-				for_x.push_back("-");
-				for_x.push_back(to_string(s[i]));
-			}
-			else if (s[i] != '+' && s[i] != '-' && s[i] != '=' && s[i] != 'x' && i == 0)
-			{
-				v.push_back("-");
-				v.push_back(to_string(s[i]));
-			}
-			else if (s[i + 1] != '+' && s[i + 1] != '-' && s[i + 1] != '=' && s[i + 1] == 'x' && s[i] == '-' && i == 0)
-			{
-				v.push_back("+");
-				v.push_back(to_string(s[i + 1]));
-			}
-			else if (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=' && i == s.size() - 1)
+			if (s[i] != '-' && s[i] != '+' && s[i] != '=' && s[i] != 'x' && i == 0)
+				v.push_back(to_string(ctoi(s[i])));
+			else if ((s[i] == '-' || s[i] == '+') && (s[i + 1] != '-' && s[i + 1] != '+' && s[i + 1] != '=' && s[i + 1] != 'x'))
 			{
 				v.push_back(to_string(s[i]));
+				v.push_back(to_string(ctoi(s[i + 1])));
+			}
+			else if (s[i] == '=')
+			{
+				for_x.push_back("1");
 				break;
 			}
-			else if (s[i] != 'x' && (s[i + 1] != 'x' && s[i + 1] != '+' && s[i + 1] != '-' && s[i + 1] != '=') && s[i] == '-')
-			{
-				v.push_back("+");
-				v.push_back(to_string(s[i + 1]));
-			}
-			else if (s[i] != 'x' && (s[i + 1] != 'x' && s[i + 1] != '+' && s[i + 1] != '-' && s[i + 1] != '=') && s[i] == '+')
-			{
-				v.push_back("-");
-				v.push_back(to_string(s[i + 1]));
-			}
-			else if (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=' && (s[i - 1] != 'x' && s[i - 1] != '+' && s[i - 1] != '-' && s[i - 1] != '=') && v.size() != 0)
-			{
-				v[v.size() - 1] += to_string(s[i]);
-			}
-			else if (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=' && v.size() == 0)
-			{
-				v.push_back("-");
+		}
+		else
+		{
+			if (s[i] == '=')
 				v.push_back(to_string(s[i]));
+			else
+			{
+				if (s[i] == 'x' && i == 0)
+					for_x.push_back("1");
+				else if (s[i + 1] == 'x' && s[i] == '+')
+					for_x.push_back("1");
+				else if (s[i + 1] == 'x' && s[i] == '-')
+					for_x.push_back("-1");
+				else if (s[i + 1] == 'x' && (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=') && i == 0)
+				{
+					char c = ctoi(s[i]);
+					for_x.push_back(to_string(c));
+				}
+				else if (s[i + 1] == 'x' && (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=') && s[i - 1] == '+')
+				{
+					char c1 = ctoi(s[i]);
+					for_x.push_back(to_string(c1));
+				}
+				else if (s[i + 1] == 'x' && (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=') && s[i - 1] == '-')
+				{
+					for_x.push_back("-");
+					char f = ctoi(s[i]);
+					for_x.push_back(to_string(f));
+				}
+				else if (s[i] != '+' && s[i] != '-' && s[i] != '=' && s[i] != 'x' && i == 0)
+				{
+					v.push_back("-");
+					char e = ctoi(s[i]);
+					v.push_back(to_string(e));
+				}
+				else if ((s[i] != '+' && s[i] != 'x' && s[i] != '-' && s[i] != '=') && (s[i + 1] != '+' && s[i + 1] != 'x' && s[i + 1] != '-' && s[i + 1] != '=') && (s[i - 1] != '=' && s[i - 1] != '+' && s[i - 1] != 'x' && s[i - 1] != '-'))
+					v.push_back(v[v.size() - 1] + to_string(ctoi(s[i - 1])) + to_string(ctoi(s[i])) + to_string(ctoi(s[i + 1])));
+				else if (s[i + 1] != '+' && s[i + 1] != '-' && s[i + 1] != '=' && s[i + 1] == 'x' && s[i] == '-' && i == 0)
+				{
+					v.push_back("+");
+					v.push_back(to_string(ctoi(s[i + 1])));
+				}
+				else if (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=' && i == s.size() - 1)
+				{
+					v.push_back(to_string(ctoi(s[i])));
+					break;
+				}
+				else if (s[i] != 'x' && (s[i + 1] != 'x' && s[i + 1] != '+' && s[i + 1] != '-' && s[i + 1] != '=') && s[i] == '-')
+				{
+					v.push_back("+");
+					v.push_back(to_string(ctoi(s[i + 1])));
+				}
+				else if (s[i] != 'x' && (s[i + 1] != 'x' && s[i + 1] != '+' && s[i + 1] != '-' && s[i + 1] != '=') && s[i] == '+')
+				{
+					v.push_back("-");
+					v.push_back(to_string(ctoi(s[i + 1])));
+				}
+				else if (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=' && (s[i - 1] != 'x' && s[i - 1] != '+' && s[i - 1] != '-' && s[i - 1] != '=') && v.size() > 1)
+				{
+					v[v.size() - 1] += to_string(ctoi(s[i]));
+				}
+				else if (s[i] != 'x' && s[i] != '+' && s[i] != '-' && s[i] != '=' && v.size() == 1)
+				{
+					v.push_back("-");
+					v.push_back(to_string(ctoi(s[i])));
+				}
 			}
 		}
 	}
@@ -86,13 +119,34 @@ int main()
 	string sum1 = "";
 	for (ll j = 0; j < v.size(); j++)
 	{
-		if (v[j] != "61")
+		if (v[j] != "61" && v[j] != "43" && v[j] != "45")
 			sum += v[j];
 		else
 		{
-			for (ll h = j + 1; h < v.size(); h++)
-				sum1 += v[h];
-			break;
+			if(v[j] == "61")
+			{
+				for (ll h = j + 1; h < v.size(); h++)
+					sum1 += v[h];
+				break;
+			}
+			else
+			{
+				for (ll k = j; k < v.size(); k++)
+				{
+					if (v[k] == "61")
+						break;
+					else
+					{
+						if (v[k] == "43")
+							sum1 += "+";
+						else if (v[k] == "45")
+							sum1 += "-";
+						else
+							sum1 += v[k];
+					}
+				}
+				break;
+			}
 		}
 	}
 	string sum_x = "";
