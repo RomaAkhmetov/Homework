103bfeaf417017eabf2ea503b40e4e17300cdb0a
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-30 12:06:38 +0300

    https://acmp.ru/index.asp?main=task&id_task=637

---
+++ 637.cpp
@@ -0,0 +1,40 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+int main()
+{
+	ll n;
+	cin >> n;
+
+	vector <ll> a(n);
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	ll k;
+	cin >> k;
+
+	ll res = 0;
+	for (ll j = 0; j < a.size(); j++)
+		res += min(a[j], k);
+	cout << res;
+}
+
+//Z
