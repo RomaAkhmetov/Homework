f031170913294fa17e8d77b638b9d8a968e2d616
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-05-31 17:34:46 +0300

    https://acmp.ru/index.asp?main=task&id_task=843

---
+++ 843.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+
+int main()
+{
+	ll n, m, f, l;
+	cin >> n >> m >> f >> l;
+
+	ll f1 = n - l - m;
+	ll m1 = n - l - f;
+	ll z = max(f, m) - max(f1, m1);
+	cout << z << ' ' << m1 << ' ' << f1;
+}
+
+//Z
