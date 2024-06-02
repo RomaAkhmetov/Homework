0f9a309e86bd39f123174da2fffae13f83bc7304
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-06-02 13:49:13 +0300

    https://acmp.ru/index.asp?main=task&id_task=706

---
+++ 706.cpp
@@ -0,0 +1,31 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+
+
+int main()
+{
+	ll r, x, y;
+	cin >> r >> x >> y;
+
+	double res = (r * abs(x)) / (2.0 * r - y);
+	cout << fixed << setprecision(2) << res;
+}
+
+//Z
