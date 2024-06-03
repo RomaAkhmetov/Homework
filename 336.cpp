ec1cae326e53615844b67b1fa514d4c7a62a91a8
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-06-03 15:46:15 +0300

    https://acmp.ru/index.asp?main=task&id_task=336

---
+++ 336.cpp
@@ -0,0 +1,55 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	ll begin = 1;
+	ll t_b = begin;
+	ll mx = t_b;
+	vector <ll> v = { t_b };
+	for (ll i = 0; i < s.size(); i++)
+	{
+		if (s[i] == '2')
+		{
+			begin--;
+			if (begin != mx && find(v.begin(), v.end(), begin) == end(v))
+			{
+				mx++;
+				v.push_back(begin);
+			}
+		}
+		else if(s[i] == '1')
+		{
+			begin++;
+			if (begin != mx && find(v.begin(), v.end(), begin) == end(v))
+			{
+				mx++;
+				v.push_back(begin);
+			}
+		}
+	}
+	cout << mx;
+}
+
+//Z
