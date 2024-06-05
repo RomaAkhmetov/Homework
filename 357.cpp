3a6f2ae65e95265eab3a393462bc458db8f90170
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-06-05 14:42:48 +0300

    https://acmp.ru/index.asp?main=task&id_task=357

---
+++ 357.cpp
@@ -0,0 +1,45 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+char ctoi(char a)
+{
+	return a - '0';
+}
+
+int main()
+{
+	string n;
+	cin >> n;
+
+	ll sum1 = 0;
+	ll sum2 = 0;
+	for (ll i = 0; i < n.size(); i++)
+	{
+		if (i % 2 == 0)
+			sum1 += ctoi(n[i]);
+		else
+			sum2 += ctoi(n[i]);
+	}
+	if (abs(sum1 - sum2) % 11 == 0)
+		cout << "YES";
+	else
+		cout << "NO";
+}
+
+//Z
