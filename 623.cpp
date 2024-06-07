8e31db2b45ac7006a771435819db96820e430924
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-06-07 11:28:02 +0300

    https://acmp.ru/index.asp?main=task&id_task=623

---
+++ 623.cpp
@@ -0,0 +1,30 @@
+﻿#define _USE_MATH_DEFINES
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+ 
+
+int main()
+{
+	ull n;
+	cin >> n;
+
+	ull Fn = round((pow((1 + sqrt(5)) / 2, n + 1) - pow((1 - sqrt(5)) / 2, n + 1)) / sqrt(5));
+	cout << Fn % 10;
+}
+
+//Z
