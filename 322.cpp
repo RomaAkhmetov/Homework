e535e4a2725298e803b528f2b84723a8edb1d36c
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-06-17 18:09:17 +0300

    https://acmp.ru/index.asp?main=task&id_task=322

---
+++ 322.cpp
@@ -0,0 +1,52 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	ll f1 = 0;
+	ll f2 = 1;
+	vector <ll> f = {1};
+	ll res = 0;
+	while (res <= s.size())
+	{
+		res = f1 + f2;
+		f.push_back(res);
+		f1 = f2;
+		f2 = res;
+	}
+	vector <ll> ind;
+	for (ll i = 1; i <= s.size(); i++)
+		ind.push_back(i);
+	string res1 = "";
+	for (ll j = 0; j < f.size(); j++)
+	{
+		if (j == 1)
+			continue;
+		else
+		{
+			if (f[j] - 1 < ind.size() && f[j] == ind[f[j] - 1])
+				res1 += s[ind[f[j] - 1] - 1];
+		}
+	}
+	cout << res1;
+}
