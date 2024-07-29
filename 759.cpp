2e26e201fa8ad3f6c5dd7ea181bb3385515ddb69
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-07-29 11:20:55 +0300

    https://acmp.ru/index.asp?main=task&id_task=759

---
+++ 759.cpp
@@ -0,0 +1,44 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n, m;
+	cin >> n >> m;
+
+	vector <ll> c(n);
+	for (ll i = 0; i < n; i++)
+		cin >> c[i];
+	sort(c.rbegin(), c.rend());
+	ll sum = 0;
+	ll j = 0;
+	for (ll p = 0; p < m; p++)
+	{
+		if (c[j] > 0)
+		{
+			sum += c[j];
+			j++;
+		}
+	}
+	cout << sum;
+}
+
+//Z
