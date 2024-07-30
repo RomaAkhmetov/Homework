0d6d05d6a58fd0e0b5c0c589c982712f4c5bd9d3
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-07-30 11:30:44 +0300

    https://acmp.ru/index.asp?main=task&id_task=296

---
+++ 296.cpp
@@ -0,0 +1,39 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    
+    for (ll i = n / 5; i >= 0; i--)
+    {
+        ll ost = n - i * 5;
+        if (ost % 3 == 0)
+        {
+            cout << i << ' ' << ost / 3;
+            return 0;
+        }
+    }
+}
+
+//Z
