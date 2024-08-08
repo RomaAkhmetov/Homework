28765d85da99f80994e64806ed1eef9649fbfb91
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-08-08 14:33:02 +0300

    https://acmp.ru/index.asp?main=task&id_task=680

---
+++ 680.cpp
@@ -0,0 +1,32 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	ll Sn = (3 * (1 - pow(2, (n - 1)))) / -1;
+	ll res = 3 + Sn;
+	cout << res;
+}
+
+//Z
