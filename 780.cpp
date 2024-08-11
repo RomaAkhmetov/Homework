8e83a26f2484167964cea9e5df513b384eb21e8c
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-08-11 12:08:07 +0300

    https://acmp.ru/index.asp?main=task&id_task=780

---
+++ 780.cpp
@@ -0,0 +1,30 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	ll D = 1 + (-4 * (-2 * n));
+	double n1 = ceil((double)(-1 + sqrt(D)) / 2);
+	cout << n1;
+}
+
+//Z
