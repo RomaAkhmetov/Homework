73023e715948f28d83dbd275e20b9c4e70c35acb
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-08-12 09:52:40 +0300

    https://acmp.ru/index.asp?main=task&id_task=633

---
+++ 633 (2).cpp
@@ -0,0 +1,36 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	string Team;
+	getline(cin, Team);
+
+	vector<string>v(3);
+	for (ll i = 0; i < 3; i++)
+		getline(cin,v[i]);
+
+	sort(v.begin(), v.end());
+
+	cout << Team << ':' << ' ' << v[0] << ',' << ' ' << v[1] << ',' << ' ' << v[2];
+
+}
+
+//Z
