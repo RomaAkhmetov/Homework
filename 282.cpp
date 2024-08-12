ff65b94de99b0cf2b0fb817ac4fca9a04113f625
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-08-12 15:52:07 +0300

    https://acmp.ru/index.asp?main=task&id_task=282

---
+++ 282.cpp
@@ -0,0 +1,30 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+int main()
+{
+	ll w, h;
+	cin >> w >> h;
+
+	ll res = (h * (h + 1) / 2) * (w * (w + 1) / 2);
+	cout << res;
+}
+
+//Z
