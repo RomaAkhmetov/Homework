6e1c8ee3a620162ec476ff39a9c3ea541fbe8222
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-08-16 11:57:19 +0300

    https://acmp.ru/index.asp?main=task&id_task=676

---
+++ 676.cpp
@@ -0,0 +1,33 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	if (n % 3 == 0)
+		cout << 2;
+	else
+		cout << 1;
+}
+
+//Z
