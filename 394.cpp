e089c1e26091a89fd839d88004824c47d8d00b73
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-08-23 15:29:11 +0300

    https://acmp.ru/index.asp?main=task&id_task=394

---
+++ 394.cpp
@@ -0,0 +1,38 @@
+﻿#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll GCD(ll a, ll b)
+{
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+    return a + b;
+}
+
+
+int main()
+{
+	ll n, m;
+	cin >> n >> m;
+
+    cout << n / GCD(n, m);
+}
