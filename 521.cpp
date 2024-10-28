8bb2af223ea35f604a5dc64bd4a4f04a6b17745b
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-10-28 12:07:18 +0300

    https://acmp.ru/index.asp?main=task&id_task=521

---
+++ 521.cpp
@@ -0,0 +1,59 @@
+﻿#define _USE_MATH_DEFINES_
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll conversionTotwo(ll p)
+{
+    ll steps = 0;
+    while (p > 2)
+    {
+        if (p % 2 == 0)
+        {
+            p /= 2;
+            steps++;
+        }
+        else if (p % 2 != 0)
+        {
+            p = p * 3 + 1;
+            steps++;
+        }
+    }
+    return steps;
+}
+
+int main()
+{
+    ll p, k;
+    cin >> p >> k;
+
+    vector<ll> a;
+    for (ll i = p; i <= k; i++)
+        a.push_back(i);
+
+    ll cnt = 0;
+    for (ll j = 0; j < a.size(); j++)
+    {
+        if (a[j] == 2)
+            continue;
+        else
+            cnt += conversionTotwo(a[j]);
+    }
+    cout << cnt;
+}
+
+//Z
