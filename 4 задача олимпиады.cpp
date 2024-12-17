2b8cfe2d2fa1b7a1f1dcb655eab3bcc3dad9dd5c
Zorro <160057959+RomaAkhmetov@users.noreply.github.com>
2024-12-17 15:58:38 +0300

    Сириус, олимпиады, 4

---
+++ 4 задача олимпиады.cpp
@@ -0,0 +1,43 @@
+﻿#define _USE_MATH_DEFINES 
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <set>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <sstream>
+#include <stack>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+int main()
+{
+	ll n;
+	cin >> n;
+
+	vector<ll> a(n);
+	for (ll p = 0; p < n; p++)
+		cin >> a[p];
+
+	ll cnt = 0;
+	for (ll i = 0; i <= (n - 2); i++)
+	{
+		for (ll j = i + 1; j <= (n - 1); j++)
+		{
+			if ((a[i] + (j + 1)) == (a[j] + (i + 1)))
+				cnt++;
+		}
+	}
+	cout << cnt;
+}
+
+//Z
